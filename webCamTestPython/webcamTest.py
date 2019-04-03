863f1fe87d46f2c40c4298085d032991f0033013
nilssonfilip1996 <nilssonfilip1996@hotmail.com>
2019-04-03 13:58:59 +0200

    Changed print

--- webCamTestPython/webcamTest.py
+++ webCamTestPython/webcamTest.py
@@ -10,6 +10,7 @@ Follow this guide in order to use dependancies: https://pypi.org/project/ar-mark
 from __future__ import print_function
 import cv2
 from ar_markers import detect_markers
+import time
 
 if __name__ == '__main__':
         print('Press "q" to quit')
@@ -22,14 +23,17 @@ if __name__ == '__main__':
                 frame_captured = False
 
         while frame_captured:
-                markers = detect_markers(frame)
-                for marker in markers:
-                        marker.highlite_marker(frame)
-                frame = cv2.resize(frame, (1920,1080))
-                cv2.imshow('Detection Frame', frame)
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                        break
-                frame_captured, frame = capture.read()
+            time.sleep(0.3) #Delay for easier console reading
+            markers = detect_markers(frame)
+            for marker in markers:
+                    marker.highlite_marker(frame)
+                    print("value: " + str(marker.id), "coordinate: " + str(marker.center))
+            print("--------------------")
+            frame = cv2.resize(frame, (1210,720))
+            cv2.imshow('Detection Frame', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+            frame_captured, frame = capture.read()
                 
         
         
