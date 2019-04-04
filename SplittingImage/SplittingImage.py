2b712e0f13fc064e03f47b253ba443561d020f1a
Aron <aron.polner@gmail.com>
2019-04-04 15:26:36 +0200

    tracking of one marker and displaying its id and center

--- SplittingImage/SplittingImage.py
+++ SplittingImage/SplittingImage.py
@@ -7,14 +7,24 @@ Created on Thu Apr  4 09:48:25 2019
 """
 
 import cv2, time
+from ar_markers import detect_markers
+from ar_markers.marker import HammingMarker
+import numpy as np
 
 # create an object. 0 for external camera
 video = cv2.VideoCapture(0)
 
+# change the serolution of the video stream
+video.set(3, 640)
+video.set(4, 460)
+
 def find_quadrant(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONUP:
-
-        print("you pressed lbuttonup")
+        print("x: {}, y: {}".format(x, y))
+        # quadrant 1
+        if x > 230 and y < 320:
+            print("first quadrant")
+            
 
 while(True):
 
@@ -22,11 +32,13 @@ while(True):
     check, frame = video.read()
     
     # convert to grayscale
-    gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = frame
 
     # capture the height and width
     height, width = frame.shape[:2]
-    #print(height)
+
+
 
     # vertical line in the middle
     start_row, start_col = int(0), int(0)
@@ -36,15 +48,17 @@ while(True):
     # horizontal line in the middle
     middle_col, start_row, end_row = int(width * .5), int(0), int(height)
     cv2.line(gray, (middle_col, start_row), (middle_col, end_row), 10)
-
-    # cropped_top = gray[start_row:end_row, start_col:end_col]
-        #print(start_row, end_row)
-        #print(start_col, end_col)
+    
+    markers = detect_markers(gray)
+    #print("length of marker data structure: ", len(markers))
+    if markers:
+        markers[0].highlite_marker(gray)
+        print("id: {}, center: {}".format(markers[0].id, markers[0].center))
 
     # show the frame
     cv2.imshow("capturing", gray)
 
-    cv2.setMouseCallback("capturing", find_quadrant)
+    cv2.setMouseCallback("capturing", find_quadrant, gray)
     
     # for playing
     key=cv2.waitKey(1)
@@ -55,4 +69,11 @@ while(True):
 # shut down camera
 video.release()
 cv2.destroyAllWindows
+
+
+
+# --------------------- 
+
+ #   font = cv2.FONT_HERSHEY_SIMPLEX
+ #   cv2.putText(gray, "Quadrant code", (200,200), font, 1, (255,255,255), 2, cv2.LINE_AA)
 
