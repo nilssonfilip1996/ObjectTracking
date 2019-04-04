0d7180e2c2b9ef480efbe44531a3bba00a396aa3
Aron <aron.polner@gmail.com>
2019-04-04 12:53:18 +0200

    split in four

--- SplittingImage/SplittingImage.py
+++ SplittingImage/SplittingImage.py
@@ -11,6 +11,11 @@ import cv2, time
 # create an object. 0 for external camera
 video = cv2.VideoCapture(0)
 
+def find_quadrant(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONUP:
+
+        print("you pressed lbuttonup")
+
 while(True):
 
     # create a fram object
@@ -21,16 +26,25 @@ while(True):
 
     # capture the height and width
     height, width = frame.shape[:2]
+    #print(height)
 
-    # frame shape is 720, 1280 - crop that
+    # vertical line in the middle
     start_row, start_col = int(0), int(0)
-    end_row, end_col = int(height * .5), int(width)
-    cropped_top = gray[start_row:end_row, start_col:end_col]
+    middle_row, end_col = int(height * .5), int(width)
+    cv2.line(gray, (start_col, middle_row),(end_col, middle_row), 10)
+
+    # horizontal line in the middle
+    middle_col, start_row, end_row = int(width * .5), int(0), int(height)
+    cv2.line(gray, (middle_col, start_row), (middle_col, end_row), 10)
+
+    # cropped_top = gray[start_row:end_row, start_col:end_col]
         #print(start_row, end_row)
         #print(start_col, end_col)
 
     # show the frame
-    cv2.imshow("capturing", cropped_top)
+    cv2.imshow("capturing", gray)
+
+    cv2.setMouseCallback("capturing", find_quadrant)
     
     # for playing
     key=cv2.waitKey(1)
